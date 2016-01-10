cb5ef109b4b9a3a9021b1a0c6d7088ff58985217
Alex Chubay <Alex.Chubay+edu@gmail.com>
2016-01-10 18:07:37 +0200

    first 3 plots

--- plot1.R
+++ plot1.R
@@ -1,11 +1,12 @@
 
 datafile <- "household_power_consumption.txt"
+isPNG <- TRUE ##  if isPNG == TRUE then print to PNG device, othrwise print to the screen
 
 if(file.exists(datafile)) {
   
     ## compute the location and size of [Feb-01'07 - Feb-02'07] range of records in the dataset
     startdt <- strptime("16/12/2006 17:24:00", "%d/%m/%Y %H:%M:%S")
-    startdtFeb01 <- strptime("1/1/2007 00:00:00", "%d/%m/%Y %H:%M:%S")
+    startdtFeb01 <- strptime("1/2/2007 00:00:00", "%d/%m/%Y %H:%M:%S")
     skiprec <- difftime(startdtFeb01, startdt, units="mins") + 1 ## add 1 to compensate for header in the file
     framesize <- 2 * 24 * 60
     
@@ -17,19 +18,23 @@ if(file.exists(datafile)) {
     headers <- read.table(datafile, header=TRUE, sep=";", nrows = 1)
     names(poweruse) <- names(headers)
     
-    ##activating PNG file device
-    png(filename="plot1.png", width=480, height=480)
+    if(isPNG) {
+          ##activating PNG file device
+          png(filename="plot1.png", width=480, height=480)  
+    }
     
-    ## printing power usage data into the historgam in the PNG file device
+    ## printing power usage data into the historgam
     ## since the range between min and max of Global_active_power is ~4, setting breaks to ~4*2
-    hist(poweruse$Global_active_power, breaks=8, col="red", 
+    hist(poweruse$Global_active_power, breaks=16, col="red", 
          main = "Global Active Power", xlab="Global Active Power (kilowatts)")
     
-    ## restoring the device back to the default value
-    dev.off()  
-    
-    print("Histogram was successfully printed. Please check plot1.png file in your current directory.")
-    
+    if (isPNG) {
+          ## restoring the device back to the default value
+          dev.off()  
+      
+          print("Histogram was successfully printed. Please check plot1.png file in your current directory.")
+    }
+
 } else {
   print("Data file is NOT found. Please make sure that it's located in the same directory as the script.")
 }
--- plot3.R
+++ plot3.R
@@ -26,18 +26,27 @@ if(file.exists(datafile)) {
     
     if(isPNG) {
       ##activating PNG file device
-      png(filename="plot2.png", width=480, height=480)  
+      png(filename="plot3.png", width=480, height=480)  
     }
     
+    ## initializing plot area (type='n')
+    with(poweruse2, plot(dt, Sub_metering_1, type="n", xlab="", 
+                         ylab="Energy sub metering"))
+    
     ## printing power usage data
-    with(poweruse2, plot(dt, Global_active_power, type="l", xlab="", 
-                         ylab="Global Active Power (kilowatts)"))
+    with(poweruse2, lines(dt, Sub_metering_1, col="black"))
+    with(poweruse2, lines(dt, Sub_metering_2, col="red"))
+    with(poweruse2, lines(dt, Sub_metering_3, col="blue"))
+    
+    ## adding legend 
+    legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+           lty=1, lwd=2, col=c("black", "red", "blue"))
     
     if (isPNG) {
       ## restoring the device back to the default value
       dev.off()  
       
-      print("The plot was successfully printed. Please check plot2.png file in your current directory.")
+      print("The plot was successfully printed. Please check plot3.png file in your current directory.")
     }
     
 } else {
